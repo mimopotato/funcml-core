a3040e32c8c244522c4cf84145a95709035aec9e
Mimopotato <173955441+mimopotato@users.noreply.github.com>
2024-07-10 16:02:33 +0200

    feat: implements sha256sum - closes #7

--- test/cryptography_test.rb
+++ test/cryptography_test.rb
@@ -21,4 +21,21 @@ class CryptographyTest < Test::Unit::TestCase
 
     assert_equal struct.mutate(mutations)[:key], "ecb252044b5ea0f679ee78ec1a12904739e2904d"
   end
+
+  test "crypto__sha256sum_returns_computed_value" do
+    struct = {key: {_sha256sum: "string"}}
+    assert_equal struct.mutate[:key], "473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8"
+  end
+
+  test "crypto__sha256sum_null_value_returns_value" do
+    struct = {key: {_sha256sum: ""}}
+    assert_equal struct.mutate[:key], "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  end
+
+  test "crypto_sha256sum_from_mutation" do
+    struct = {key: {_sha256sum: "$value"}}
+    mutations = {value: "string"}
+
+    assert_equal struct.mutate(mutations)[:key], "473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8"
+  end
 end
