a8b03292d5b9b6dbe5c57be355cd9f450619a3dc
Mimopotato <173955441+mimopotato@users.noreply.github.com>
2024-07-10 18:15:20 +0200

    feat: implements ago

--- lib/funcml/patch/funcs/time.rb
+++ lib/funcml/patch/funcs/time.rb
@@ -3,6 +3,8 @@
 require "time"
 
 class Hash
+  include Funcml
+
   def _time(mutations)
     self.fetch(:_time).then do |blk|
       _value = blk.fetch(:value)
@@ -11,6 +13,16 @@ class Hash
       _value.mutate(mutations).then do |value|
         return Time.parse(value).strftime(_format.mutate(mutations))
       end
+    end
+  end
+
+  def _ago(mutations)
+    self.fetch(:_ago).then do |blk|
+      unless blk.is_a?(Integer)
+        raise IncorrectSecondsException, "_ago only supports seconds as value in #{self.to_s}"
+      end
+
+      (Time.now - blk).to_s
     end
   end
 end
