d003059e05a4bfdd664dd3faa910888f49b17573
Mimopotato <173955441+mimopotato@users.noreply.github.com>
2024-07-10 19:04:50 +0200

    chore(structure): list instead of dic

--- lib/funcml/patch/funcs/dictionary.rb
+++ lib/funcml/patch/funcs/dictionary.rb
@@ -24,22 +24,4 @@ class Hash
       end.flatten
     end
   end
-
-  def _first(mutations)
-    self.fetch(:_first).mutate(mutations).then do |elems|
-      return elems.first
-    end
-  end
-
-  def _tail(mutations)
-    self.fetch(:_tail).mutate(mutations).then do |elems|
-      return elems[0].last(elems[1])
-    end
-  end
-
-  def _head(mutations)
-    self.fetch(:_head).mutate(mutations).then do |elems|
-      return elems[0][0..(elems[1]-1)]
-    end
-  end
 end
