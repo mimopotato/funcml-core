94e597afd12928fc0acc558d80ca640de90b8013
Mimopotato <173955441+mimopotato@users.noreply.github.com>
2024-07-10 13:08:57 +0200

    feat: _base64decode implementation closes #3

--- test/encoding_test.rb
+++ test/encoding_test.rb
@@ -8,10 +8,20 @@ class EncodingTest < Test::Unit::TestCase
   test "encoding__base64encode_returns_encoded_string" do
     struct = {key: {_base64encode: "string"}}
     assert_equal struct.mutate[:key], "c3RyaW5n"
-end
+  end
 
-test "encoding__base64encode_null_string_returns_empty" do
+  test "encoding__base64encode_null_string_returns_empty" do
     struct = {key: {_base64encode: ""}}
+    assert_equal struct.mutate[:key], ""
+  end
+
+  test "encoding__base64decode_returns_decoded_string" do
+    struct = {key: {_base64decode: "c3RyaW5n"}}
+    assert_equal struct.mutate[:key], "string"
+  end
+
+  test "encoding__base64decode_null_string_returns_empty" do
+    struct = {key: {_base64decode: ""}}
     assert_equal struct.mutate[:key], ""
   end
 end
