5e2766e64a9916cc313bebf8eaf74732b1c05fdb
Mimopotato <173955441+mimopotato@users.noreply.github.com>
2024-07-10 13:08:23 +0200

    chore: separate empty string test

--- test/encoding_test.rb
+++ test/encoding_test.rb
@@ -8,7 +8,9 @@ class EncodingTest < Test::Unit::TestCase
   test "encoding__base64encode_returns_encoded_string" do
     struct = {key: {_base64encode: "string"}}
     assert_equal struct.mutate[:key], "c3RyaW5n"
+end
 
+test "encoding__base64encode_null_string_returns_empty" do
     struct = {key: {_base64encode: ""}}
     assert_equal struct.mutate[:key], ""
   end
