f129236dc4670d667539b75c0b83bc2c6d743f5c
Mimopotato <173955441+mimopotato@users.noreply.github.com>
2024-07-12 11:48:06 +0200

    test: math__div

--- test/math_test.rb
+++ test/math_test.rb
@@ -20,4 +20,12 @@ class MathTest < Test::Unit::TestCase
     struct = {_sub: ["$first", "$second"]}
     assert_equal struct.mutate(first: 2, second: 1), 1
   end
+
+  test "math__div_mutates_calling_block" do
+    struct = {_div: [3.0, 2.0]}
+    assert_equal struct.mutate, 1.5
+
+    struct = {_div: ["$first", "$second"]}
+    assert_equal struct.mutate(first: 3.0, second: 2.0), 1.5
+  end
 end
