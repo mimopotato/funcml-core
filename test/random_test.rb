a2d2f1e2b90cdc74db04d07e27d67fcf4797ad4f
mimopotato <173955441+mimopotato@users.noreply.github.com>
2024-08-04 14:14:50 +0200

    feat(random): implements _randomString, closes #78

--- test/random_test.rb
+++ test/random_test.rb
@@ -23,4 +23,28 @@ class RandomTest < Test::Unit::TestCase
     struct = {key: {_randomNumber: {min: 1.0, max: 2.0}}}
     assert_equal struct.mutate[:key].class, Float
   end
+
+  test "random__randomString" do
+    100.times do
+      struct = {key: {_randomString: {length: 6}}}
+      result = struct.mutate[:key]
+      assert_equal result.length, 6
+    end
+  end
+
+  test "random__randomString_whitelist" do
+    100.times do
+      struct = {key: {
+        _randomString: {
+          length: 6,
+          allowed: "abc"
+        }
+      }}
+
+      result = struct.mutate[:key]
+      result.chars.each do |char|
+        assert !"defhjklmnopqrstuvwxyz".chars.include?(char)
+      end
+    end
+  end
 end
